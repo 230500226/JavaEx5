83a34302a08ed363fb90dc0578bd19e8a1b3b8a6
230500226 <125464717+230500226@users.noreply.github.com>
2024-08-14 10:22:00 +0200

    init netbeans

---
+++ src/main/java/com/mycompany/javaex5/JavaEx5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaex5;
+
+/**
+ *
+ * @author ldxt460s
+ */
+public class JavaEx5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
