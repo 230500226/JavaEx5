f00da70b70857c88b7d04436b418305828c6ce08
230500226 <125464717+230500226@users.noreply.github.com>
2024-08-21 11:16:24 +0200

    actually recursive now

--- src/main/java/com/mycompany/javaex5/Fibonacci.java
+++ src/main/java/com/mycompany/javaex5/Fibonacci.java
@@ -10,15 +10,17 @@ package com.mycompany.javaex5;
  */
 public class Fibonacci {
 
-    public static void start() {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public void start() {
         int n = 40;
-        int a = 0, b = 1, c;
-        System.out.print("Fibonacci Series of " + n + " numbers: ");
-        for (int i = 1; i <= n; i++) {
-            System.out.print(a + " ");
-            c = a + b;
-            a = b;
-            b = c;
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
         }
     }
     
